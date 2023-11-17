e57f795f80d9cfc945728608b188c1f0950177c3
youabledev <youable.framios@gmail.com>
2023-11-18 02:59:11 +0900

    add: first and last operation

--- study/study/Combine/SequenceOperationTest.swift
+++ study/study/Combine/SequenceOperationTest.swift
@@ -12,9 +12,9 @@ import Combine
 class SequenceOperationTestViewModel {
     @ObservationIgnored var subject = PassthroughSubject<Int, Error>()
     @ObservationIgnored var cancelable = Set<AnyCancellable>()
-    @ObservationIgnored let items: [Int] = Array(0..<3)
     
     var data: [String] = []
+    var errorMessage: String = ""
     
     init() {
         
@@ -23,7 +23,7 @@ class SequenceOperationTestViewModel {
     func startFirstOperation() {
         subject
             .map { String($0) }
-            .first()
+            .first() // 가장 첫번째 요소만
             .sink { _ in
             } receiveValue: { [weak self] value in
                 self?.data.append(value)
@@ -33,12 +33,69 @@ class SequenceOperationTestViewModel {
         publishData()
     }
     
+    func startFirstWithWhereOperation() {
+        subject
+            .first { $0 > 4 } // 해당 조건에 맞는 값 중 첫번째만
+            .map { String($0) }
+            .sink { _ in
+            } receiveValue: { [weak self] value in
+                self?.data.append(value)
+            }
+            .store(in: &cancelable)
+        
+        publishData()
+    }
+    
+    func startTryFirstOperation() {
+        subject
+            .tryFirst {
+                if $0 == 1 {
+                    throw URLError(.badServerResponse) // 에러 던질 수 있음
+                }
+                return $0 > 4 // 조건에 맞는 첫번째 요소만 반환
+                // return $0 > 1 // show 2
+            }
+            .map { String($0) }
+            .sink { completion in
+                switch completion {
+                case .finished:
+                    print("finished tryFirst")
+                case .failure(let error):
+                    self.errorMessage = error.localizedDescription // error handling
+                }
+            } receiveValue: { [weak self] value in
+                self?.data.append(value)
+            }
+            .store(in: &cancelable)
+        
+        publishData()
+    }
+    
+    func startLast() {
+        subject
+            .last()
+            .map { String($0) }
+            .sink { _ in
+            } receiveValue: { [weak self] value in
+                self?.data.append(value)
+            }
+            .store(in: &cancelable)
+        
+        publishData()
+    }
+    
+    // MARK: - publishing
     private func publishData() {
         data.removeAll()
         
+        let items: [Int] = Array(0..<10)
         for index in items.indices {
             DispatchQueue.main.asyncAfter(deadline: .now() + Double(index)) {
                 self.subject.send(index)
+                
+                if index == items.count {
+                    self.subject.send(completion: .finished) // finished를 보내면 게시 가능한 상태에서 completion 상태가 된다. (last는 마지막 값을 호출하므로 completion을 보내야 한다)
+                }
             }
         }
     }
@@ -49,12 +106,31 @@ struct SequenceOperationTest: View {
     
     var body: some View {
         VStack {
-            HStack {
-                Button("first") {
-                    vm.startFirstOperation()
+            VStack {
+                HStack {
+                    Button("first") {
+                        vm.startFirstOperation()
+                    }
+                    .buttonStyle(.borderedProminent)
+                    
+                    Button("first with where") {
+                        vm.startFirstWithWhereOperation()
+                    }
+                    .buttonStyle(.borderedProminent)
+                    
+                    Button("try first") {
+                        vm.startTryFirstOperation()
+                    }
+                    .buttonStyle(.borderedProminent)
+                } //: HStack
+                
+                HStack {
+                    Button("last") {
+                        vm.startLast()
+                    }
+                    .buttonStyle(.borderedProminent)
                 }
-                .buttonStyle(.borderedProminent)
-            } //: HStack
+            } //: VStack
             
             ScrollView {
                 VStack {
@@ -63,6 +139,8 @@ struct SequenceOperationTest: View {
                     }
                 } //: VStack
             } //: ScrollView
+            Text(vm.errorMessage)
+                .foregroundStyle(.red)
         } //: VStack
     }
 }
