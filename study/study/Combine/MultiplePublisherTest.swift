07d68891ee3597d398e113ed9101e3849889f7d1
youabledev <youable.framios@gmail.com>
2023-11-20 00:30:07 +0900

    add: combineLatest, merge

--- study/study/Combine/MultiplePublisherTest.swift
+++ study/study/Combine/MultiplePublisherTest.swift
@@ -15,17 +15,27 @@ class MultiplePublisherTestViewModel: ObservableObject {
     
     @Published var data: [String] = []
     
+    @Published var address: String = ""
+    @Published var detailAddress: String = ""
+    @Published var fullAddress: String = ""
+    
     init() {
-        
+        // combineLatest test
+        Publishers.CombineLatest($address, $detailAddress)
+            .map {
+                "\($0) \($1)"
+            }
+            .assign(to: \.fullAddress, on: self)
+            .store(in: &cancellable)
     }
     
     func testCombineLatest() {
         stringPublisher
-            .combineLatest(boolPublisher)
+            .combineLatest(boolPublisher) // 두 개 이상의 publisher들이 발행하는 최신 값을 결합하여 새로운 값을 생성함
             .map { string, bool in
-                return "\(string)  \(bool)"
+                // 어느 한 publisher에서 새로운 값이 방출될 때마다 다른 publisher의 가장 최근 값과 결합하여 새로운 결과를 방출함
+                return "\(string)  \(bool)" // 각 publisher가 최소 한 번씩의 값을 방출한 후에 결합된 값을 방출하게 됨
             }
-//            .removeDuplicates()
             .sink { completion in
                 switch completion {
                 case .finished:
@@ -49,7 +59,37 @@ class MultiplePublisherTestViewModel: ObservableObject {
                 self.stringPublisher.send(items[index])
             }
         }
-            
+
+    }
+    
+    func testMerge() {
+        // 여러 publisher로 부터 발행된 값들을 단일 publisher로 병합하는데 사용함
+        // 발행된 값들을 순서 관계 없이 그대로 방출
+        let firstSubject = PassthroughSubject<String, Never>()
+        let secondSubject = PassthroughSubject<String, Never>()
+        
+        firstSubject
+            .merge(with: secondSubject)
+            .sink { [weak self] value in
+                self?.data.append(value)
+            }
+            .store(in: &cancellable)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            firstSubject.send("가")
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            firstSubject.send("나")
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            secondSubject.send("다")
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 4) {
+            firstSubject.send("라")
+        }
     }
 }
 
@@ -58,16 +98,28 @@ struct MultiplePublisherTest: View {
     
     var body: some View {
         ScrollView {
-            Button("combine latest") {
-                vm.testCombineLatest()
+            HStack {
+                Button("combine latest") {
+                    vm.testCombineLatest()
+                }
+                .buttonStyle(.borderedProminent)
+                
+                Button("merge") {
+                    vm.testMerge()
+                }
+                .buttonStyle(.borderedProminent)
             }
-            .buttonStyle(.borderedProminent)
+            
             
             VStack {
                 ForEach(vm.data, id: \.self) {
                     Text($0)
                 }
             } //: VStack
+            
+            TextField("주소 입력", text: $vm.address)
+            TextField("상세 주소 입력", text: $vm.detailAddress)
+            Text("주소 : \(vm.fullAddress)")
         }
     }
 }
